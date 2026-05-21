bc172674c03ea9b08d412c10bf08146a0ee4d2f5
Vivk1560 <vivaankukreja44@gmail.com>
2026-05-21 13:08:50 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,18 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/static/',
+        ],
+      },
+    ],
     sitemap: 'https://www.mahabaleshwarvillastays.com/sitemap.xml',
+    host: 'https://www.mahabaleshwarvillastays.com',
   }
 }
